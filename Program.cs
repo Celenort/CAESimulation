e215cbbbc605f859e022eac5285703c67a2cd136
celenort <celenort@seoul.ac.kr>
2022-07-20 01:15:02 +0900

    test commit

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace CAESimulation
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CAESimulation());
+            //Test commit
         }
     }
 }
